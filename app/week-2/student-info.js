fab8b9b6136675c792215209fdbfa68cbe287777
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-09-05 12:48:38 -0600

    student info

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <div>
+        <h1>My name is Airzy Turqueza</h1>
+        <Link href="https://github.com/Air-zy/cprg306-assignments">Github Repo</Link>
+    </div>
+  );
+}
