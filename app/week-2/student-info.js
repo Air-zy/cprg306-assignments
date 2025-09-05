3c87e5527c6a0209349b8fe1b16b1b8ba69304e1
Airzy <120424474+Air-zy@users.noreply.github.com>
2025-09-05 13:13:38 -0600

    adjustment

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -3,8 +3,11 @@ import Link from "next/link";
 export default function StudentInfo() {
   return (
     <div>
-        <h1>My name is Airzy Turqueza</h1>
-        <Link href="https://github.com/Air-zy/cprg306-assignments">link: Github Repo</Link>
+        <p>Name: Airzy Turqueza</p>
+        <p>
+          GitHub:
+          <Link href="https://github.com/Air-zy/cprg306-assignments">Air-zy/cprg306-assignments</Link>
+        </p>
     </div>
   );
 }
